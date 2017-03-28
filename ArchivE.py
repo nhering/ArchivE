17f092d3f85c92450f808319dccff039183972b7
nhering <nhering1@gmail.com>
2017-03-28 12:39:26 -0700

    Completed version 1

--- ArchivE.py
+++ ArchivE.py
@@ -7,13 +7,13 @@ import os
 from os import listdir
 import time
 
-# Currently set to 24 hours ago. This can be used later to accept a user defined time for validating if a file should be archived or not.
-archiveTime = time.time() - 86400
 
-mod_or_create = ""
-instructions = "Step 1: Select the source directory\nStep 2: Select the destination directory\nStep 3: Select desired option for archiving\nStep 4: Press the 'Archive Now' button to perfom the archive"
-srcdir = "Please select a source folder."
-dstdir = ""
+archive_time = time.time() - 86400
+instructions="Step 1: Select the source directory\nStep 2: Select the destination directory\nStep 3: Select desired option for archiving\nStep 4: Press the 'Archive Now' button to perfom the archive"
+srcdir="Please select a source folder."
+dstdir=""
+criteria=""
+archive_list=[]
 
 class Archive:
     def __init__(self, master):
@@ -26,9 +26,6 @@ class Archive:
         self.instruct=ttk.Label(ins, text=instructions, justify=LEFT, width = 65)
         self.instruct.grid(padx=5, pady=5, sticky=W, row=0, column=0)
 
-        self.archive_btn=ttk.Button(ins, text="Archive\nNow", state=DISABLED, command="")
-        self.archive_btn.grid(pady=5, row=0, column=1, sticky=NE)
-
 
         #Paths----------------------------------------------------------------------------------------------------------
         paths=ttk.Frame(master)
@@ -37,7 +34,7 @@ class Archive:
         self.src_btn=ttk.Button(paths, text="Source", command=lambda: select_src(self))
         self.src_btn.grid(pady=5, row=0, column=0)
         self.src_label = ttk.Label(paths, text=srcdir, justify=LEFT, width=64)
-        self.src_label.grid(padx=5, pady=5, sticky=W, row=0, column=1, columnspan=2)
+        self.src_label.grid(padx=5, pady=5, sticky=SW, row=0, column=1, columnspan=2)
 
         self.dst_btn=ttk.Button(paths, text="Destination", command=lambda: select_dst(self))
         self.dst_label=ttk.Label(paths, justify=LEFT, width=64, text=dstdir)
@@ -56,6 +53,10 @@ class Archive:
 
         def validate_paths(self):
             if (srcdir == ""):
+                self.archive_btn.config(state=DISABLED)
+                self.display_files.config(state='normal')
+                self.display_files.delete('1.0', 'end')
+                self.display_files.config(state='disabled')
                 if (dstdir == ""):
                     self.src_label.config(text="Please select a source folder.")
                     self.dst_label.config(text="")
@@ -63,33 +64,90 @@ class Archive:
                     self.src_label.config(text="Please select a source folder.")
             else:
                 self.dst_btn.grid(pady=5, row=1, column=0)
-                self.dst_label.grid(padx=5, pady=5, sticky=W, row=1, column=1, columnspan=2)
+                self.dst_label.grid(padx=5, pady=5, sticky=SW, row=1, column=1, columnspan=2)
+                populate_list(self)
                 if (dstdir == ""):
+                    self.archive_btn.config(state=DISABLED)
                     self.dst_label.config(text="Please select a destination folder.")
                 elif (srcdir == dstdir):
+                    self.archive_btn.config(state=DISABLED)
                     messagebox.showinfo("Archive: Path error",
                                         "You may not select the same folder for both the source and destination.")
                 else:
                     options.grid(padx=5, pady=5, sticky=W, row=2, column=0)
+                    self.archive_btn.config(state=ACTIVE)
 
 
         #Options--------------------------------------------------------------------------------------------------------
-        options=ttk.Frame(master, width=600)
+        options=ttk.Frame(master)
+
+        mod_or_create = ""
 
         self.options_text=ttk.Label(options, text="Select archive option.")
         self.options_text.grid(pady=5, sticky=W, row=0, column=0, columnspan=3)
 
-        self.mod_create_opt1=ttk.Radiobutton(options, text = "Only files that have been modified in the past 24 hours.", variable=mod_or_create, value="m")
-        self.mod_create_opt1.grid(padx=2, pady=1, sticky=W, row=1, column=0)
+        self.mod_create_opt1=ttk.Radiobutton(options,
+                                             text = "Files that have been MODIFIED in the past 24 hours.",
+                                             variable=mod_or_create, value=1, command=lambda: options_set(self,option="m"))
+        self.mod_create_opt1.grid(padx=2, sticky=W, row=1, column=0)
 
-        self.mod_create_opt2=ttk.Radiobutton(options, text = "Only files that have been created in the past 24 hours.", variable=mod_or_create, value="c")
-        self.mod_create_opt2.grid(padx=2, pady=1, sticky=W, row=2, column=0)
+        self.mod_create_opt2=ttk.Radiobutton(options,
+                                             text = "Files that have been CREATED in the past 24 hours.",
+                                             variable=mod_or_create, value=2, command=lambda: options_set(self,option="c"))
+        self.mod_create_opt2.grid(padx=2, sticky=W, row=2, column=0)
 
-        self.mod_create_opt3=ttk.Radiobutton(options, text = "Files that have been either modified or created in the past 24 hours.", variable=mod_or_create, value="mc")
-        self.mod_create_opt3.grid(padx=2, pady=1, sticky=W, row=3, column=0)
-
+        def options_set(self,option):
+            global criteria
+            criteria=option
+            feedback.grid(padx=5, row=3, column=0, sticky=W)
+            populate_list(self)
 
         #Feedback-------------------------------------------------------------------------------------------------------
+        feedback=ttk.Frame(master)
+
+        ttk.Label(feedback, text="The following files will be copied").grid(row=0, column=1, sticky=W, pady=5)
+
+        self.display_files=Text(feedback, width=50, height=10, state='disabled')
+        self.display_files.grid(pady=5, row=1, column=1, sticky=W)
+
+        self.archive_btn=ttk.Button(feedback, text="Archive Now", state=DISABLED, command=lambda: archive(self))
+        self.archive_btn.grid(pady=5, row=2, column=1, sticky=W)
+
+        def archive(self):
+            global archive_list
+            for i in archive_list:
+                print(i+" copied")
+                shutil.copyfile((srcdir + "\\" + i), (dstdir + "\\" + i))
+            populate_list(self)
+
+        def populate_list(self):
+            global criteria
+            global archive_list
+            archive_list=[]
+            files_in_folder=listdir(srcdir)
+            self.display_files.config(state='normal')
+            self.display_files.delete('1.0', 'end')
+            self.display_files.config(state='disabled')
+
+            def display_file(i):
+                self.display_files.config(state='normal')
+                self.display_files.insert('1.0', i+'\n')
+                self.display_files.config(state='disabled')
+
+            if (criteria == 'm'):
+                for i in files_in_folder:
+                    file_time = os.path.getmtime(srcdir + "\\" + i)
+                    if (file_time > archive_time):
+                        archive_list.append(i)
+                        display_file(i)
+            elif (criteria == 'c'):
+                for i in files_in_folder:
+                    file_time = os.path.getctime(srcdir + "\\" + i)
+                    if (file_time > archive_time):
+                        archive_list.append(i)
+                        display_file(i)
+
+
 
 
 
